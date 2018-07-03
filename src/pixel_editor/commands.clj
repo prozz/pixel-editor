445cc0ad91a442d2dc56b2cc6097998dff4eea29
prozz <prozzz@gmail.com>
2018-07-03 11:49:54 +0200

    no need for commands atom

--- src/pixel_editor/commands.clj
+++ src/pixel_editor/commands.clj
@@ -1,59 +1,115 @@
 (ns pixel-editor.commands
   "commands definitions used by command line interface"
   (:require [clojure.string :as str]
-            [pixel-editor.commands-core :refer :all]
             [pixel-editor.core :as c]
             [pixel-editor.utils :as u]
             [pixel-editor.validators :as v]))
+
+(def current-image (atom nil))
+
+(defn command
+  "command with given params"
+  [desc validator command]
+  {:desc desc
+   :validator validator
+   :command command})
+
+(defn normalize
+  "adjusting to indexing from 0, needed by all commands but new-image"
+  [args]
+  (map u/decrement args))
+
+(defn apply-on-image
+  "fires function only when image exists"
+  [f]
+  (if (nil? @current-image)
+    (throw (ex-info "No image." {}))
+    (f)))
+
+(defn update-image!
+  "wrapper for image updating function, changes image state, command args are normalized by default"
+  ([f]
+   (update-image! f normalize))
+  ([f af]
+   (fn [image args] (apply-on-image #(swap! image f (af args))))))
+
+(defn reset-image!
+  "wrapper for image resetting function, changes image state"
+  [f]
+  (fn [image args] (reset! image (apply f args))))
+
+(defn with-image
+  "wrapper for image using function, doesn't change image state"
+  [f]
+  (fn [image args] (apply-on-image #(f @image args))))
+
+(defn fire
+  "wrapper for any, non image related, function"
+  [f]
+  (fn [image args] (f)))
+
+(defn image-args-validator
+  "wrapper for validator function, with image and args as params"
+  [f]
+  (fn [image args] (f image args)))
+
+(defn args-validator
+  "wrapper for validator function, with just args as param"
+  [f]
+  (fn [image args] (f args)))
+
+(declare commands)
+
+(defn run-command
+  "validates and runs command, throws ex-info on failed validation"
+  [name args]
+  (let [cmd (get commands name)]
+    (if ((:validator cmd) @current-image args)
+      ((:command cmd) current-image args)
+      (throw (ex-info (str "Syntax error: " (:desc cmd)) {})))))
+
+(defn get-descriptions
+  "gets all commands descriptions, useful for building help info"
+  []
+  (map :desc commands))
 
 (defn print-help
   []
   (println (str/join "\n" (get-descriptions))))
 
-(defn init!
-  "initialize all commands"
-  []
-  (command! "I"
-    "I <width> <height> - creates new image with given dimensions (from 1 to 250)"
-    (args-validator v/new-image?)
-    (reset-image! c/create-image))
+(def commands
+  {"I" (command "I <width> <height> - creates new image with given dimensions (from 1 to 250)"
+                (args-validator v/new-image?)
+                (reset-image! c/create-image))
 
-  (command! "S"
-    "S - show the content of the current image"
-    (args-validator empty?)
-    (with-image (fn [image args] (println (str "=>\n" (c/image->str image))))))
+   "S" (command "S - show the content of the current image"
+                (args-validator empty?)
+                (with-image (fn [image args] (println (str "=>\n" (c/image->str image))))))
 
-  (command! "C"
-    "C - clears current image"
-    (args-validator empty?)
-    (update-image! (fn [image args] (c/clear image))))
+   "C" (command "C - clears current image"
+                (args-validator empty?)
+                (update-image! (fn [image args] (c/clear image))))
 
-  (command! "L"
-    "L <x> <y> <colour> - colours a pixel located at (<x>, <y>) with <colour>"
-    (image-args-validator v/pixel-with-colour?)
-    (update-image! c/colour))
+   "L" (command "L <x> <y> <colour> - colours a pixel located at (<x>, <y>) with <colour>"
+                (image-args-validator v/pixel-with-colour?)
+                (update-image! c/colour))
 
-  (command! "V"
-    "V <x> <y1> <y2> <colour> - draws vertical line of given <colour> in column <x> from row <y1> to <y2>"
-    (image-args-validator v/vertical-line?)
-    (update-image! c/vertical-line))
+   "V" (command "V <x> <y1> <y2> <colour> - draws vertical line of given <colour> in column <x> from row <y1> to <y2>"
+                (image-args-validator v/vertical-line?)
+                (update-image! c/vertical-line))
 
-  (command! "H"
-    "H <x1> <x2> <y> <colour> - draws horizontal line of given <colour> in row <y> from column <x1> to <x2>"
-    (image-args-validator v/horizontal-line?)
-    (update-image! c/horizontal-line))
+   "H" (command "H <x1> <x2> <y> <colour> - draws horizontal line of given <colour> in row <y> from column <x1> to <x2>"
+                (image-args-validator v/horizontal-line?)
+                (update-image! c/horizontal-line))
 
-  (command! "F"
-    "F <x> <y> <colour> - fills nearby pixels with same colour, starting with pixel located at (<x>, <y>) with <colour>"
-    (image-args-validator v/pixel-with-colour?)
-    (update-image! c/fill-region))
+   "F" (command "F <x> <y> <colour> - fills nearby pixels with same colour, starting with pixel located at (<x>, <y>) with <colour>"
+                (image-args-validator v/pixel-with-colour?)
+                (update-image! c/fill-region))
 
-  (command! "Q"
-    "Q, ctrl-d - quit"
-    (args-validator empty?)
-    (fire u/bye))
+   "Q" (command "Q, ctrl-d - quit"
+                (args-validator empty?)
+                (fire u/bye))
 
-  (command! "help"
-    "help - prints help message"
-    (args-validator empty?)
-    (fire print-help)))
+   "help" (command "help - prints help message"
+                   (args-validator empty?)
+                   (fire print-help))})
--- test/pixel_editor/commands_test.clj
+++ test/pixel_editor/commands_test.clj
@@ -1,47 +1,45 @@
 (ns pixel-editor.commands-test
   (:require [clojure.test :refer :all]
-            [pixel-editor.commands-core :refer [run-command current-image]]
             [pixel-editor.commands :as cmd]
             [pixel-editor.core :refer [image->str]]))
 
 (deftest integration
-  (cmd/init!)
   (testing "real scenario #1"
     ; > I 4 7 
     ; > L 2 3 T
     ; > L 1 4 G
     ; > F 1 1 Y
-    (run-command "I" [4 7])
-    (run-command "L" [2 3 "T"])
-    (run-command "L" [1 4 "G"])
-    (run-command "F" [1 1 "Y"])
+    (cmd/run-command "I" [4 7])
+    (cmd/run-command "L" [2 3 "T"])
+    (cmd/run-command "L" [1 4 "G"])
+    (cmd/run-command "F" [1 1 "Y"])
     (is (= (str "YYYY\n"
                 "YYYY\n"
                 "YTYY\n"
                 "GYYY\n"
                 "YYYY\n"
                 "YYYY\n"
-                "YYYY") (image->str @current-image))))
+                "YYYY") (image->str @cmd/current-image))))
   (testing "from docs"
     ; > I 5 6
     ; > L 2 3 A
-    (run-command "I" [5 6])
-    (run-command "L" [2 3 "A"])
+    (cmd/run-command "I" [5 6])
+    (cmd/run-command "L" [2 3 "A"])
     (is (= (str "OOOOO\n"
                 "OOOOO\n"
                 "OAOOO\n"
                 "OOOOO\n"
                 "OOOOO\n"
-                "OOOOO") (image->str @current-image)))
+                "OOOOO") (image->str @cmd/current-image)))
     ; > F 3 3 J
     ; > V 2 3 4 W
     ; > H 3 4 2 Z
-    (run-command "F" [3 3 "J"])
-    (run-command "V" [2 3 4 "W"])
-    (run-command "H" [3 4 2 "Z"])
+    (cmd/run-command "F" [3 3 "J"])
+    (cmd/run-command "V" [2 3 4 "W"])
+    (cmd/run-command "H" [3 4 2 "Z"])
     (is (= (str "JJJJJ\n"
                 "JJZZJ\n"
                 "JWJJJ\n"
                 "JWJJJ\n"
                 "JJJJJ\n"
-                "JJJJJ") (image->str @current-image)))))
+                "JJJJJ") (image->str @cmd/current-image)))))
